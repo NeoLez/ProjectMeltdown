2a55813cf9e085357440bd1a17227a8824b854ad
OyitJustAShark <francosamaniego05@gmail.com>
2026-05-21 21:17:39 -0300

    merchant fix

--- Assets/NPCs/MerchantTrigger.cs
+++ Assets/NPCs/MerchantTrigger.cs
@@ -4,20 +4,20 @@ using UnityEngine;
     {
     [SerializeField] GameObject _face;
 
-    Animator _headAnimator;
+    Animator _anim;
 
     void Start()
         {
-            _headAnimator = _face.GetComponent<Animator>();
+            _anim = _face.GetComponent<Animator>();
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            _headAnimator.Play("Appear");
+            _anim.SetBool ("Appear", true);
         }
         private void OnTriggerExit(Collider other)
         {
-        _headAnimator.Play("Leave");
-    }
+            _anim.SetBool("Appear", false);
+        }
     }
 
