bc717deae900784d7e254709ea2145c58ffaeeac
TheMordex <melidonienzo@gmail.com>
2026-05-23 22:37:50 -0300

    Now everything is turned off with the power button.

--- Assets/Scripts/BatterySlot.cs
+++ Assets/Scripts/BatterySlot.cs
@@ -1,27 +1,72 @@
 using System;
 using UnityEngine;
 
-namespace Root {
-    public class BatterySlot : MonoBehaviour {
+namespace Root
+{
+    public class BatterySlot : MonoBehaviour
+    {
         [SerializeField] private Transform pivot;
+        [SerializeField] private Train train;
+
         private Battery _battery;
-        private void OnTriggerEnter(Collider other) {
-            if (other.gameObject.TryGetComponent<Battery>(out var battery)) {
+
+        public event Action OnBatteryInserted;
+        public event Action OnBatteryRemoved;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_battery != null)
+                return;
+
+            if (other.gameObject.TryGetComponent<Battery>(out var battery))
+            {
                 _battery = battery;
+
                 var rb = battery.GetComponent<Rigidbody>();
+
                 rb.constraints = RigidbodyConstraints.FreezeAll;
                 rb.position = pivot.position;
+
+
+                OnBatteryInserted?.Invoke();
             }
         }
 
-        private void LateUpdate() {
-            if (_battery != null) {
+        private void Update()
+        {
+            if (_battery == null)
+            {
+                train.SetEnginePower(false);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (_battery != null)
+            {
                 _battery.transform.position = pivot.position;
                 _battery.transform.rotation = pivot.rotation;
             }
         }
 
-        public Battery GetBattery() {
+        public void RemoveBattery()
+        {
+            if (_battery == null)
+                return;
+
+            var rb = _battery.GetComponent<Rigidbody>();
+
+            rb.constraints = RigidbodyConstraints.None;
+
+            _battery = null;
+
+            train.SetEnginePower(false);
+
+            OnBatteryRemoved?.Invoke();
+        }
+
+        public Battery GetBattery()
+        {
             return _battery;
         }
     }
