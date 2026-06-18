381b33bfe701b8c2ae062c473b74f19befe657af
OyitJustAShark <francosamaniego05@gmail.com>
2026-06-17 22:25:15 -0300

    liquido freno porcentaje fix

--- Assets/Scripts/Train/LiquidVIsualContainer.cs
+++ Assets/Scripts/Train/LiquidVIsualContainer.cs
@@ -7,11 +7,14 @@ namespace Root
         [SerializeField] TrainBrakeController _controller;
         private Renderer targetRenderer;
         private Material targetMaterial;
-
+        //-0.95, 0.1  + 0.95 both = 0, 1.1
+        float _max = 1.05f;
+        float _current;
         private float Amount = Shader.PropertyToID("_FillAmount");
 
         void Start()
         {
+            _current = _max;
             targetRenderer = GetComponent<Renderer>();
             targetMaterial = targetRenderer.material;
         }
@@ -19,8 +22,9 @@ namespace Root
         void Update()
         {
             float dynamicValue = _controller.GetDamagePercentage();
-            var a = dynamicValue * -1f;
-            targetMaterial.SetFloat(Shader.PropertyToID("_FillAmount"), a);
+            var a = _max - (_max * dynamicValue);
+            _current = a;
+            targetMaterial.SetFloat(Shader.PropertyToID("_FillAmount"), _current -0.95f);
         }
     }
 }
