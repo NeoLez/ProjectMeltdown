a5a8bfaed00c327078568720af0e0b8264cf1e73
OyitJustAShark <francosamaniego05@gmail.com>
2026-06-22 13:15:19 -0300

    fix collider

--- Assets/Scripts/Train/TriggerAnimationByInteract.cs
+++ Assets/Scripts/Train/TriggerAnimationByInteract.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Root
@@ -6,13 +7,27 @@ namespace Root
     {
         Animator _animator;
         [SerializeField] GameObject _visual;
+        [SerializeField] bool _MultipleColliders = false;
+        [SerializeField] List<BoxCollider> colliders;
         private void Start()
         {
             _animator = _visual.GetComponent<Animator>();
         }
         public override void Interact()
         {
-            Debug.Log("abrir");
+            if (_MultipleColliders)
+            {
+                if (colliders[0].enabled)
+                {
+                    colliders[0].enabled = false;
+                    colliders[1].enabled = true;
+                }
+                else
+                {
+                    colliders[0].enabled = true;
+                    colliders[1].enabled = false;
+                }
+            }
             if (_animator.GetBool("Open") == true) { _animator.SetBool("Open", false); }
             else {_animator.SetBool("Open", true); }
         }
