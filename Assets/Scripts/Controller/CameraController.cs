9e67ff70e877df93afa1c772d5e4c724c97f3ee4
Joki843 <joacohb2005@gmail.com>
2026-08-03 16:50:12 -0300

    lo que agus me dijo quie borres

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Timers;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
