a4ed17a6d7aba7f2e7e3b73a424836cda8d68442
NeoLez <leogonzachi@gmail.com>
2026-08-18 00:36:12 -0300

    Dithering strength now decreases with distance. (Panini projection disabled until I figure out how to match the distortion on the depth texture)

--- Assets/Scripts/Controller/MovementController.cs
+++ Assets/Scripts/Controller/MovementController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -75,6 +74,8 @@ namespace Root.Controller
             Vector3 worldMoveDir = (_cameraController.GetHorizontalDirectionForwardVector() * input.y +
                                     _cameraController.GetHorizontalDirectionRightVector() * input.x).Swizzle_x0y();
 
+            
+            
             _rb.linearVelocity = worldMoveDir * (_input.Movement.Run.IsPressed() ? runningMovementSpeed : movementSpeed) + Vector3.up * currentVerticalSpeed;
 
             if (_currentState == CharacterState.Grounded && _rb.linearVelocity.magnitude > 0.1f)
@@ -154,6 +155,30 @@ namespace Root.Controller
                 jump = true;
             }
         }
+        
+        /*public void AdjustMovementSpeedWithSlope() {
+            if (_currentSurfaceNormal != Vector3.zero && _currentSurfaceNormal != Vector3.up) {
+                float slopeSpeedCoefficient = angleBasedSpeedLimit.Evaluate(_currentSlopeAngle / 90f);
+            
+                //Calculates the two basis vectors for the new reference system that is relative to the plane rotation
+                Vector3 planeOppositeX = Vector3.ProjectOnPlane(_currentSurfaceNormal, Vector3.up);
+                Vector3 planeOppositeY = planeOppositeX.Swizzle_zyx();
+                planeOppositeY.x *= -1;
+            
+                //Transforms the speed to that system (matrix multiplication)
+                _speed = new Vector3(planeOppositeX.x * _speed.x + planeOppositeY.x * _speed.z, _speed.y, planeOppositeX.z * _speed.x + planeOppositeY.z * _speed.z);
+            
+                //Adjusts the component of the velocity that goes toward the plane
+                if (_speed.x > 0)
+                    _speed.x *= slopeSpeedCoefficient;
+
+                //Calculates the inverse of the matrix above, and uses it to go back to the normal basis
+                float det = planeOppositeX.x * planeOppositeY.z - planeOppositeY.x * planeOppositeX.z;
+                planeOppositeX /= det;
+                planeOppositeY /= det;
+                _speed = new Vector3(planeOppositeY.z * _speed.x - planeOppositeY.x * _speed.z, _speed.y, -planeOppositeX.z * _speed.x + planeOppositeX.x * _speed.z);
+            }
+        }*/
 
         public CharacterState GetState()
         {
