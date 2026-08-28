66f8c1a961b9ba32be2ba3a0ac517ebb586eeb06
NeoLez <leogonzachi@gmail.com>
2026-08-28 15:44:56 -0300

    Let there be order!

--- Assets/Scripts/Liquid.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Root
-{
-    public class Liquid : MonoBehaviour
-    {
-        public float rechargeAmount; 
-    }
-}
--- Assets/Scripts/MovingPlatform.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace Root {
-    public abstract class MovingPlatform : MonoBehaviour {
-        public abstract Vector3 GetSpeed();
-    }
-}
