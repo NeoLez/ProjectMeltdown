NeoLez/ProjectMeltdown

commit e2557456f92f4faaee8b5117715d802170e3a5b0

Item initialization delay

Assets/Scripts/Items/Packages/PackageStampGenerator.cs
@@ -15,10 +15,12 @@ namespace Root
         private int width;
         private int height;
         private CanvasScaler _canvasScaler;
+        private Canvas _canvas;
 
         private void Awake()
         {
             _canvasScaler = GetComponent<CanvasScaler>();
+            _canvas = GetComponent<Canvas>();
             Instance = this;
         }
 
@@ -72,7 +74,7 @@ namespace Root
 
         public void EnableCanvas(bool state)
         {
-            gameObject.SetActive(state);
+            _canvas.enabled = state;
         }
 
         private void SetDisplayValue(float value)

Assets/Scripts/Items/Packages/PackagesSystemController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -49,7 +48,7 @@ namespace Root
             GameObject[] availablePackages = perpetrator.Mission.AvailablePackages;
             for (int i = 0; i < amountToSpawn; i++)
             {
-                int randomIndex = UnityEngine.Random.Range(0, availablePackages.Length);
+                int randomIndex = Random.Range(0, availablePackages.Length);
                 PhysicalItem item = availablePackages[randomIndex].GetComponent<PhysicalItem>();
                 GameObject prefab = item.ItemState.ItemSo.CreatePhysicalItem().gameObject;
 
@@ -58,14 +57,11 @@ namespace Root
 
                 DeliveryPackageItem currentPackage = prefab.GetComponent<DeliveryPackageItem>();
                 _currentSpawnedPackages.Add(currentPackage);
+                InitializePackges(currentPackage);
 
                 yield return new WaitForSeconds(fixedSpawnTime);
 
                 newPos += Vector3.up * verticalOffset;
-
-                InitializePackges(currentPackage);
-
-                yield return new WaitForSeconds(0.1f);
             }
 
             packageStampGenerator.EnableCanvas(false);

Assets/Resources/Settings/LoggerSettingsSO.asset
@@ -12,6 +12,6 @@ MonoBehaviour:
   m_Script: {fileID: 11500000, guid: 1d5f5e0ebb8f4d21bb803ce0185a7a08, type: 3}
   m_Name: LoggerSettingsSO
   m_EditorClassIdentifier: Assembly-CSharp::Root.Log.LoggerSettingsSO
-  typesToLog: 
+  typesToLog: 05000000
   severitiesToLog: 00000000010000000200000003000000
   update: 0

Assets/Scene/Train 1.unity
@@ -806,7 +806,6 @@ MonoBehaviour:
   tunnelForkLeftGeneratorSo: {fileID: 11400000, guid: 95a7af21de327afe39a653dabcf8e2c0, type: 2}
   tunnelForkRightGeneratorSo: {fileID: 11400000, guid: 613fd7b6022dabf40a147d28bd81a677, type: 2}
   tunnelJoinGeneratorSo: {fileID: 11400000, guid: 0bf1c427ba17ed644b22fe32e0970e71, type: 2}
-  _sectionGeneratorSo: {fileID: 0}
   countUntilRebase: 15
 --- !u!4 &100897461
 Transform:
@@ -6738,7 +6737,6 @@ GameObject:
   - component: {fileID: 1248385422}
   - component: {fileID: 1248385421}
   - component: {fileID: 1248385420}
-  - component: {fileID: 1248385419}
   - component: {fileID: 1248385424}
   m_Layer: 31
   m_Name: CanvasPackageInfo
@@ -6746,24 +6744,7 @@ GameObject:
   m_Icon: {fileID: 0}
   m_NavMeshLayer: 0
   m_StaticEditorFlags: 0
-  m_IsActive: 0
---- !u!114 &1248385419
-MonoBehaviour:
-  m_ObjectHideFlags: 0
-  m_CorrespondingSourceObject: {fileID: 0}
-  m_PrefabInstance: {fileID: 0}
-  m_PrefabAsset: {fileID: 0}
-  m_GameObject: {fileID: 1248385418}
-  m_Enabled: 1
-  m_EditorHideFlags: 0
-  m_Script: {fileID: 11500000, guid: dc42784cf147c0c48a680349fa168899, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: UnityEngine.UI::UnityEngine.UI.GraphicRaycaster
-  m_IgnoreReversedGraphics: 1
-  m_BlockingObjects: 0
-  m_BlockingMask:
-    serializedVersion: 2
-    m_Bits: 4294967295
+  m_IsActive: 1
 --- !u!114 &1248385420
 MonoBehaviour:
   m_ObjectHideFlags: 0
@@ -6794,7 +6775,7 @@ Canvas:
   m_PrefabInstance: {fileID: 0}
   m_PrefabAsset: {fileID: 0}
   m_GameObject: {fileID: 1248385418}
-  m_Enabled: 1
+  m_Enabled: 0
   serializedVersion: 3
   m_RenderMode: 1
   m_Camera: {fileID: 201410344}

Assets/Scripts/Items/Inventory/InventoryItemDisplay.cs
@@ -1,9 +1,11 @@
+using Root.Log;
 using Timers;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using LogType = Root.Log.LogType;
 
 namespace Root {
     public class InventoryItemDisplay : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler {
@@ -177,14 +179,17 @@ namespace Root {
         }
 
         private bool TryGetWorldDragReceiver(out IItemDragReceiver receiver) {
+            Log.Logger.Log("Trying to get world item receiver", LogType.DragAndDrop, LogSeverity.Normal, this);
             receiver = null;
             var mousePosition = Pointer.current.position.value;
             Ray ray = GameManager.CameraPivot.ScreenPointToRay(mousePosition/GameManager.GetResolutionRatio());
             if (!Physics.Raycast(ray, out var hit, GameManager.CameraController.interactDistance) ||
                 !hit.collider.gameObject.TryGetComponent(out receiver)) {
+                Log.Logger.Log( $"Could not find receiver. Name: {hit.collider?.name}. Component was not found", LogType.DragAndDrop, LogSeverity.Normal, hit.collider?.gameObject);
                 return false;
             }
 
+            Log.Logger.Log( $"Receiver found. Name: {hit.collider.name}. Component was found", LogType.DragAndDrop, LogSeverity.Normal, hit.collider.gameObject);
             return true;
         }
 

Assets/Scripts/Log/LogType.cs
@@ -7,5 +7,6 @@
         WorldGen,
         Pool,
         Test,
+        DragAndDrop,
     }
 }

Assets/Scripts/MapGeneration/MapGeneration.cs
@@ -81,7 +81,7 @@ namespace Root {
         }
         
 
-        public SectionGeneratorSO _sectionGeneratorSo;
+        private SectionGeneratorSO _sectionGeneratorSo;
         private MapSection section;
         private int mapSectionID;
         

commit 31a767cc9742128f9d958390cd230ac357655003

Made EnableCanvas private to simplify PackageStampGenerator api. Fixes bug introduced in e2557456 where the first package generated didn't render the stamp properly.

Assets/Scripts/Items/Packages/PackageStampGenerator.cs
@@ -44,7 +44,9 @@ namespace Root
             var package = obj.GetComponent<DeliveryPackageItem>();
             SetDisplayValue(package.GetPrice());
 
+            EnableCanvas(true);
             OneShotRenderSystem.Instance.Render(render);
+            EnableCanvas(false);
             return render;
         }
 
@@ -57,7 +59,7 @@ namespace Root
             heldItemProjector.fadeFactor = 1.0f;
         }
 
-        public void EnableCanvas(bool state)
+        private void EnableCanvas(bool state)
         {
             _canvas.enabled = state;
         }

Assets/Scripts/Items/Packages/PackagesSystemController.cs
@@ -38,8 +38,6 @@ namespace Root
             }
 
             _packageGenerationRoutine = StartCoroutine(GeneratePackages(perpetrator, instancePivot, amount));
-
-            packageStampGenerator.EnableCanvas(true);
         }
 
         private IEnumerator GeneratePackages(NPCInteraction perpetrator, Transform instancePivot, int amountToSpawn)
@@ -63,8 +61,6 @@ namespace Root
 
                 newPos += Vector3.up * verticalOffset;
             }
-
-            packageStampGenerator.EnableCanvas(false);
 
             _visuals.ActivateNotification();
             _visuals.SetNewObjective(perpetrator.Mission);